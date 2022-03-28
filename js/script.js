1de935d510417e3b5653c6c85e9573eba5e900dc
Julien Herou <julien.herou@gmail.com>
2022-03-28 16:47:57 +0200

    Hover effect ok ou presque

--- js/script.js
+++ js/script.js
@@ -2,32 +2,38 @@ window.addEventListener('load', () => {
 });
 
 const persons = document.querySelectorAll('.person__image');
+const person1 = document.querySelector('.person-nb1');
+const person2 = document.querySelector('.person-nb2');
+const person3 = document.querySelector('.person-nb3');
 const personElt = document.querySelector('.person');
-const mainImgElt = document.querySelector('.main-img');
 
 persons.forEach(element => {
-    // element.addEventListener('mouseover', showImage);
-    // element.addEventListener('mouseout', replaceImage);
+    element.addEventListener('mouseover', showImage);
 });
 
 function showImage(){
-    this.style.width = '58%';
-
+    
+    if (this.classList.contains('person-nb1')) {
+        this.style.width = '58%';
+        person2.style.width = '21%';
+        person3.style.width = '21%';
+    } else if (this.classList.contains('person-nb2')) {
+        this.style.width = '58%';
+        person1.style.width = '21%';
+        person3.style.width = '21%';
+    } else if (this.classList.contains('person-nb3')) {
+        this.style.width = '58%';
+        person1.style.width = '21%';
+        person2.style.width = '21%';
+    }
 }
-function replaceImage(){
-    this.style.width = '21%';
-    // if (this.dataset.img != "1"){
-    //     console.log('pouette');
-    // }
 
-}
+
 
 personElt.addEventListener('mouseout', (event) => {
-    mainImgElt.style.width = '58%';
-    console.log('pouette');
-    event.stopPropagation();
-
-    
+    person1.style.width = '58%';
+    person2.style.width = '21%';
+    person3.style.width = '21%';
 });
 
 
